90b05a7c39282e01666e08a7acbedcd6715dd5dd
PUGE <mmit7750>
2017-03-08 22:55:44 +0800

    Saturday perfect

--- RemoteDesktop/app/src/renderer/components/join.vue
+++ RemoteDesktop/app/src/renderer/components/join.vue
@@ -5,6 +5,46 @@
         .ok 加入
 </template>
 
+<script>
+    const desktopCapturer = require('electron').desktopCapturer,
+          createPeerConnection = require('./js/peer.js'),
+          mdns = require('multicast-dns')(),
+          connect = require('./js/connect.js');
+    export default {
+        data(){
+            return{
+                windowList: {},
+                sources:{},
+                peerConnection:null
+            }
+        },
+        created:function(){
+            const _this=this;
+            mdns.on('query', function (query) {
+                console.log(query)
+            });
+            mdns.on('response', function (res) {
+                console.log(res)
+            });
+            var interval = setInterval(query, 1000)
+            query()
+            connect.verifyUserRoom(peerConnection, ui, function (err, room, config) {
+                console.log("sd");
+                clearInterval(interval)
+                if (err) {
+                    ui.inputs.paste.value = 'Error! ' + err.message
+                    return
+                }
+                ui.inputs.paste.value = 'Waiting on other side...'
+                ipc.send('create-window', {config: config, room: room})
+            })
+            function query () {
+                mdns.query([{type: 'TXT', name: 'screencat'}])
+            }
+        },
+    }
+</script>
+
 <style lang="less">
 .join-box{
     height: 125px;
