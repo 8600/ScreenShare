6d76bfa8022cce7b3442506204191a5be3729da9
蒲鸽 <my@owo.help>
2017-05-13 13:02:53 +0800

    es6

--- RemoteDesktop/app/src/renderer/components/getRoom.vue
+++
@@ -1,9 +0,0 @@
-<template lang="pug">
-  input(autofocus)
-</template>
-<style lang="less">
-
-</style>
-<script>
-
-</script>
--- screencat-master/electron.js
+++ screencat-master/electron.js
@@ -26,44 +26,41 @@ mainWindow.on('ready', ()=> {
 
 //监听icon事件
 ipc.on('icon', (ev, key)=> {
-  let icon = null 
+  let icon = null;
   switch(key){
     case 'connected':    icon = `${__dirname}/img/IconRed.png`; break;
     case 'disconnected': icon = `${__dirname}/img/IconRed.png`; break;
     default: console.log(`[icon]收到未知信号！`)
   }
   mainWindow.tray.setImage(icon)
-})
+});
 
-mainWindow.app.on('open-url', function (e, lnk) {
-  console.log("mainWindow.window")
-  e.preventDefault()
-  console.log(mainWindow.window)
-  if (mainWindow.window) mainWindow.window.webContents.send('open-url', lnk)
-})
-
-ipc.on('terminate', function terminate (ev) {
+//监听结束事件
+ipc.on('terminate', (ev)=> {
   mainWindow.app.quit()
-})
+});
 
-ipc.on('resize', function resize (ev, data) {
+//监听改变窗口大小事件
+ipc.on('resize', (ev, data)=> {
   mainWindow.window.setSize(data.width, data.height)
-})
+});
 
-ipc.on('error', function error (ev, err) {
+//监听发生错误事件
+ipc.on('error', (ev, err)=> {
   console.error(new Error(err.message))
-})
+});
 
-ipc.on('create-window', function (ev, config) {
-  console.log('create-window', [config])
-  mainWindow.app.dock.show()
-  win = new BrowserWindow({width: 720, height: 445})
-  win.loadURL('file://' + path.join(__dirname, 'screen.html'))
+//监听创建窗口事件
+ipc.on('create-window', (ev, config)=> {
+  console.log('窗口新窗口', [config]);
+  mainWindow.app.dock.show(); //显示应用在 dock 中的图标[MAC]
+  win = new BrowserWindow({width: 720, height: 445}); //创建新窗口
+  win.loadURL(`file://${__dirname}/screen.html`);
 
-  win.on('closed', function () {
-    mainWindow.app.dock.hide()
-    mainWindow.window.webContents.send('disconnected', true)
-  })
+  win.on('closed', ()=> {
+    mainWindow.app.dock.hide(); //隐藏应用在 dock 中的图标。[MAC]
+    mainWindow.window.webContents.send('disconnected', true); //发送连接关闭消息
+  });
 
   ipc.once('window-ready', function () {
     // win.webContents.openDevTools()
